efb93ed6b60d9898b6535c079ae1d5b5f6ca503a
U-ALLOCATESOFTWAR\Filip.Gligorov <Filip.Gligorov@SKP-L3230.allocatesoftware.com>
2018-12-27 11:44:05 +0100

    initial commit

---
+++ selenium_firefox.py
@@ -0,0 +1,35 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from sys import argv
+
+username = argv[1]
+password = argv[2]
+start = argv[3]
+finish = argv[4]
+
+firefoxProfile = FirefoxProfile()
+firefoxProfile.set_preference("plugin.state.flash", 2)
+driver = webdriver.Firefox(firefoxProfile, executable_path='C:/cygwin64/home/Filip.Gligorov/requests/geckodriver.exe')
+url = "http://sdc-devvatfxap1/HealthRoster/FLEXAUTOPS/"
+driver.get(url)
+
+username = driver.find_element_by_id("txtUsername")
+password = driver.find_element_by_id("txtPassword")
+
+username.send_keys(username)
+password.send_keys(password)
+driver.find_element_by_id("btnLogin").click()
+
+request_cookie_browser = driver.get_cookie('.HRFedAuth_3601_FLEXAUTOPS')
+cookies={'.HRFedAuth_3601_FLEXAUTOPS':str(request_cookie_browser['value'])}
+
+s = requests.Session()
+
+for i in range(int(start),int(finish)+1):
+	body = '<BulkSendDutyToBank IsPartOfBlock="false"> <SendDutiesToBank> <SendDutyToBank ID="',i,'" FulfilledOnlyByNHSpWebPortal="false"> <OrgUnit ID="',finish,'"/> <BookingReason ID="1"/> <Competences/> </SendDutyToBank> </SendDutiesToBank> </BulkSendDutyToBank>'
+	print body
+	response = s.post('http://sdc-devvatfxap1/HealthRoster/FLEXAUTOPS/Process', cookies=cookies, data=body)
+
+s.close()
+driver.quit()
