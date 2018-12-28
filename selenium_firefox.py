7496407975ce789b19b11b2ddb42b06cb0bc6c9b
U-ALLOCATESOFTWAR\Filip.Gligorov <Filip.Gligorov@SKP-L3230.allocatesoftware.com>
2018-12-28 16:38:26 +0100

    run the duties

--- selenium_firefox.py
+++ selenium_firefox.py
@@ -13,28 +13,29 @@ finish = argv[4]
 print "User: ", username
 print "Passwd: ", password
 
-# firefoxProfile = FirefoxProfile()
-# firefoxProfile.set_preference("plugin.state.flash", 2)
-# driver = webdriver.Firefox(firefoxProfile, executable_path='C:/cygwin64/home/Filip.Gligorov/requests/geckodriver.exe')
-# url = "http://sdc-devvatfxap1/HealthRoster/FLEXAUTOPS/"
-# driver.get(url)
+firefoxProfile = FirefoxProfile()
+firefoxProfile.set_preference("plugin.state.flash", 2)
+driver = webdriver.Firefox(firefoxProfile, executable_path='C:/cygwin64/home/Filip.Gligorov/requests/geckodriver.exe')
+url = "http://sdc-devvatfxap1/HealthRoster/FLEXAUTOPS/"
+driver.get(url)
 
-# username = driver.find_element_by_id("txtUsername")
-# password = driver.find_element_by_id("txtPassword")
+username = driver.find_element_by_id("txtUsername")
+password = driver.find_element_by_id("txtPassword")
 
-# username.send_keys(username)
-# password.send_keys(password)
-# driver.find_element_by_id("btnLogin").click()
+username.send_keys(username)
+password.send_keys(password)
+driver.find_element_by_id("btnLogin").click()
 
-# request_cookie_browser = driver.get_cookie('.HRFedAuth_3601_FLEXAUTOPS')
-# cookies={'.HRFedAuth_3601_FLEXAUTOPS':str(request_cookie_browser['value'])}
+request_cookie_browser = driver.get_cookie('.HRFedAuth_3601_FLEXAUTOPS')
+cookies={'.HRFedAuth_3601_FLEXAUTOPS':str(request_cookie_browser['value'])}
 
-# s = requests.Session()
+s = requests.Session()
 
 for i in range(int(start),int(finish)+1):
 	body = '<BulkSendDutyToBank IsPartOfBlock="false"> <SendDutiesToBank> <SendDutyToBank ID="',i,'" FulfilledOnlyByNHSpWebPortal="false"> <OrgUnit ID="',argv[5],'"/> <BookingReason ID="1"/> <Competences/> </SendDutyToBank> </SendDutiesToBank> </BulkSendDutyToBank>'
 	print body
-# 	response = s.post('http://sdc-devvatfxap1/HealthRoster/FLEXAUTOPS/Process', cookies=cookies, data=body)
+	response = s.post('http://sdc-devvatfxap1/HealthRoster/FLEXAUTOPS/Process', cookies=cookies, data=body)
+	print response.content
 
-# s.close()
-# driver.quit()
+s.close()
+driver.quit()
